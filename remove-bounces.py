6c30aba9fa580930a94b243e630299bd71570089
Steve Gledhill <stevegleds@users.noreply.github.com>
2016-12-15 14:43:54 +0000

    15 December 2016 Added code to 'remove bounces to check date on files'

--- remove-bounces.py
+++ remove-bounces.py
@@ -1,9 +1,23 @@
 import re
 import csv
 import os
+import datetime
+old_file = False
 bounces_filename = 'bounces.mbox'  # this is a test file
 bounces_file = os.path.join('', bounces_filename)
-print("filename is: ", bounces_file)
+bounces_modified_date = datetime.date.fromtimestamp(os.path.getmtime(bounces_file))
+print('today is:', datetime.date.today(), 'file is: ', bounces_modified_date)
+if datetime.date.today() != bounces_modified_date:
+    old_file = True
+print("filename is: ", bounces_file, "modified on :", datetime.date.fromtimestamp(os.path.getmtime(bounces_file)))
+if old_file:
+    print('Your file is old, are you sure you want to continue?')
+    response = input('Enter Y to continue. Any other input will quit.')
+    if response != 'Y':
+        raise SystemExit
+
+output_filename = 'bounces.csv'
+output_file = os.path.join('', output_filename)
 count = 0
 countbrackets = 0
 countrecipients = 0
@@ -42,7 +56,7 @@ print('We found ', count + countbrackets + countrecipients, 'email addresses and
 emails = list(set(emails))  # Remove duplicates
 print('We removed ', count + countbrackets + countrecipients - len(emails), 'duplicates' )
 emails.insert(0, 'email')  # Add title to first row for import
-emailsfile = open('bounced_emails.csv', 'w', newline='')
+emailsfile = open(output_file, 'w', newline='')
 wr = csv.writer(emailsfile, quoting=csv.QUOTE_ALL)
 for email in emails:
     wr.writerow([email])
